deb03f755ef213de52ba988646bcbeaa55876451
Liu Junxiao <jxltom@gmail.com>
2018-07-12 11:08:30 +0800

    Add handler for base exceptions

--- nodebin/blueprints/apiv1/hooks.py
+++ nodebin/blueprints/apiv1/hooks.py
@@ -5,5 +5,10 @@ from .exceptions import ApiException
 
 
 @apiv1.errorhandler(ApiException)
-def platform_not_found_exception(exception):
+def api_exception(exception):
     return jsonify(exception.to_dict()), exception.status_code
+
+
+@apiv1.errorhandler(Exception)
+def all_exception(exception):
+    return jsonify(dict(code=500, message=str(exception))), 500
